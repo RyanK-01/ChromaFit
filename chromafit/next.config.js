68da784384e394fbd8cd225f241e0fd919507ca0
liesehaha <kohy0100@e.ntu.edu.sg>
2025-10-19 03:52:58 +0800

    Remove stray next.config.js (using next.config.ts)

--- chromafit/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Using the stable version of Next.js
-  experimental: {
-    turbopack: false
-  }
-}
-
-module.exports = nextConfig
